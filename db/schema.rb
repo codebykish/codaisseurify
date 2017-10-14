a6137923889bfaf3f240f4c2e0d793e6733fbd1b
codebykish <kishan_letters@yahoo.com>
2017-10-15 01:02:20 +0200

    Create Association Artist Song

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014224324) do
+ActiveRecord::Schema.define(version: 20171014230104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 20171014224324) do
     t.string "song_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
+  add_foreign_key "songs", "artists"
 end
