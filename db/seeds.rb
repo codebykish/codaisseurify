4263ab579388808c9d606dd4f73ca837fe2cfa95
codebykish <kishan_letters@yahoo.com>
2017-10-15 02:12:15 +0200

    Add Database Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Artist.destroy_all
+Song.destroy_all
+
+artist_1 = Artist.create!(artist_name: "Wu-Tang", age:40, bio:"Wu-Tang makes songs.")
+artist_2 = Artist.create!(artist_name: "ODB", age:35, bio:"ODB makes songs.")
+artist_3 = Artist.create!(artist_name: "Rza", age:18, bio:"Rza makes songs.")
+artist_4 = Artist.create!(artist_name: "Gza", age:34, bio:"Gza makes songs.")
+artist_5 = Artist.create!(artist_name: "Ghostface", age:47, bio:"Ghostface makes songs.")
+artist_6 = Artist.create!(artist_name: "Method Man", age:55, bio:"Method Man makes songs.")
+
+song_1 = Song.create!(song_name: "Happy", artist: artist_1)
+song_2 = Song.create!(song_name: "Sad", artist: artist_1)
+song_3 = Song.create!(song_name: "Yellow", artist: artist_3)
+song_4 = Song.create!(song_name: "Blue", artist: artist_3)
+song_5 = Song.create!(song_name: "Green", artist: artist_5)
+song_6 = Song.create!(song_name: "Brown", artist: artist_5)
+song_7 = Song.create!(song_name: "Black", artist: artist_6)
+song_8 = Song.create!(song_name: "Estatic", artist: artist_2)
+song_9 = Song.create!(song_name: "Red", artist: artist_2)
+song_10 = Song.create!(song_name: "Purple", artist: artist_2)
+song_11 = Song.create!(song_name: "Not Sure", artist: artist_4)
