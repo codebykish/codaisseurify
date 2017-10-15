51c43f6c5657c34c7053b23f8d343a68775d2d4f
codebykish <kishan_letters@yahoo.com>
2017-10-15 16:37:04 +0200

    Added Songs Form - Add and Edit

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,45 +1,50 @@
 class SongsController < ApplicationController
 
   def index
-    @songs = artist.songs
+    @songs = Song.all
   end
 
-  def show; end
-
-  def new
-    @song = artist.songs.build
-  end
-
-  def create
-    @song = artist.songs.build(song_params)
-
-    if @song.save
-      redirect_to @song, notice: "Room created"
-    else
-      render :new
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @song.update(song_params)
-      redirect_to @song, notice: "Room updated"
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def set_song
+  def show
     @song = Song.find(params[:id])
   end
 
-  def song_params
-    params
-      .require(:song_name)
+  def edit
+    @song = Song.find(params[:id])
   end
-end
+
+  def destroy
+    @song = Song.find(params[:id])
+
+    @song.destroy
+
+    redirect_to songs_path
+  end
+
+  def create
+
+    @song = Song.new(song_params)
+
+    if @song.save
+       redirect_to @song
+    else
+       render 'new'
+    end
+  end
+
+    def update
+    @song = Song.find(params[:id])
+
+    if @song.update_attributes(song_params)
+       redirect_to @song
+    else
+       render 'edit'
+    end
+  end
+
+   private
+
+   def song_params
+    params.require(:song).permit(:song_name, :artist_id)
+   end
 
 end
