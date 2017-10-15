20b208966c1d8b27ade818ed519c1b5f88b4a06c
codebykish <kishan_letters@yahoo.com>
2017-10-15 05:49:10 +0200

    Added Destroy in Controller and View: Artist Show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,4 +8,12 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
   end
 
+  def destroy
+    @artist = Artist.find(params[:id])
+
+    @artist.destroy
+
+    redirect_to artists_path
+  end
+
 end
