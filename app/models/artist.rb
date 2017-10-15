e5fca6b9928c020b543c8756d49250c9c8706726
codebykish <kishan_letters@yahoo.com>
2017-10-15 17:50:31 +0200

    Added Validations To Artist Model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,6 @@ class Artist < ApplicationRecord
   has_many :photos, dependent: :destroy
 
   validates :artist_name, presence: true
+  validates :bio, length: { maximum: 50 }, allow_blank: true
+  validates :image_url, length: { maximum: 255 }, allow_blank: true
 end
